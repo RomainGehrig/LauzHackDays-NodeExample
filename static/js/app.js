49c5d05e32fbd4a9f21d6313a310d7e1433b61b3
Romain Gehrig <romain.gehrig@gmail.com>
2019-10-21 08:46:08 +0200

    Implement JS functions to retrieve and show comments

---
+++ static/js/app.js
@@ -0,0 +1,73 @@
+"use strict";
+
+function createElementForComment(commentJson) {
+  const container = document.createElement("div");
+  container.setAttribute("class", "clearfix shadow bg-light mb-2 p-2 rounded");
+  const text = document.createElement("p");
+  text.setAttribute("class", "float-left m-0");
+  text.innerHTML = commentJson["text"];
+  const name = document.createElement("span");
+  name.setAttribute("class", "float-right font-weight-bold");
+  name.innerHTML = `${commentJson["user"]} (${timestampToString(
+    commentJson["time"]
+  )})`;
+  container.appendChild(text);
+  container.appendChild(name);
+  return container;
+}
+
+function timestampToString(ts) {
+  return new Date(ts).toLocaleTimeString(navigator.language, {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false
+  });
+}
+
+async function getComments() {
+  return jsonGet("/api/comments").then(comments => {
+    const commentsDiv = document.getElementById("comments");
+
+    // TODO Do something useful with the comments !
+    console.log(comments);
+
+    /**
+       <div class="clearfix shadow bg-light mb-2 p-2 rounded">
+         <p class="float-left m-0">
+           Hello, I'm a comment
+         </p>
+         <span class="float-right font-weight-bold">
+           Anonymous - 12:38
+         </span>
+       </div>
+    */
+
+    for (const comment of comments) {
+      const commentElem = createElementForComment(comment);
+      commentsDiv.appendChild(commentElem);
+    }
+  });
+}
+
+// Helper functions for GET and POST
+async function jsonQuery(method, endpoint, body) {
+  return fetch(endpoint, {
+    method: method,
+    mode: "cors",
+    body: body,
+    cache: "no-cache",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  }).then(resp => resp.json());
+}
+
+async function jsonGet(endpoint) {
+  return jsonQuery("GET", endpoint, null);
+}
+
+async function jsonPost(endpoint, jsonData) {
+  return jsonQuery("POST", endpoint, jsonData);
+}
